f15ee82eee6091a3f95c7f81e49f7408222175ae
mrsanjays <mrsanjays133@gmail.com>
2023-07-14 13:07:33 +0530

    main

---
+++ Equilibrium_Index_of_Array.py
@@ -0,0 +1,27 @@
+class Solution:
+    @staticmethod
+    def Equilibrium(array):
+        n=len(array)
+        psum=[0]*n
+        psum[0]=array[0]
+        for i in range(1,n):
+            psum[i]=psum[i-1]+array[i]
+        for i in range(1,n):
+            if psum[i-1]==psum[n-1]-psum[i]:
+                return i
+        return -1
+"""
+You are given an array A of integers of size N.
+Your task is to find the equilibrium index of the given array
+The equilibrium index of an array is an index such that the sum of elements at lower indexes is equal to the sum of elements at higher indexes.
+If there are no elements that are at lower indexes or at higher indexes, then the corresponding sum of elements is considered as 0.
+
+PS:
+If there is no equilibrium index then return -1.
+If there are more than one equilibrium indexes then return the minimum index.
+"""
+if __name__ == '__main__':
+    ob=Solution()
+    array=list(map(int,input().split()))
+    print(ob.Equilibrium(array))
+
